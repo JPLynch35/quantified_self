1046e19cadaec354fcb0913c4a88858660447552
JP Lynch <JPLynch35@gmail.com>
2018-10-02 13:25:41 -0600

    add api endpoint for food with id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'foods', to: 'foods#index'
+      resources :foods, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v1/food_spec.rb
+++ spec/requests/api/v1/food_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe 'Food API' do
+  before :each do
+    Food.create(name: 'banana', calories: 150)
+    Food.create(name: 'apple', calories: 100)
+    Food.create(name: 'toast', calories: 75)
+    Food.create(name: 'strawberries', calories: 200)
+  end
   context 'GET /api/v1/foods' do
-    before :each do
-      Food.create(name: 'banana', calories: 150)
-      Food.create(name: 'apple', calories: 100)
-      Food.create(name: 'toast', calories: 75)
-      Food.create(name: 'strawberries', calories: 200)
-    end
     it 'returns a list of all foods' do
       get '/api/v1/foods'
 
@@ -19,6 +19,18 @@ describe 'Food API' do
       expect(foods.count).to eq(4)
       expect(food[:name]).to eq('banana')
       expect(food[:calories]).to eq(150)
+    end
+  end
+  context 'GET /api/v1/foods/:id' do
+    it 'returns the food with corresponding id' do
+      get '/api/v1/foods/2'
+
+      expect(response).to be_successful
+
+      food = JSON.parse(response.body, symbolize_names: true)
+
+      expect(food[:name]).to eq('apple')
+      expect(food[:calories]).to eq(100)
     end
   end
 end
