464c2162fda3ff4cd84c23469a59fef374da5b5d
JP Lynch <JPLynch35@gmail.com>
2018-10-02 15:33:38 -0600

    Add sad path testing

--- spec/requests/api/v1/food_spec.rb
+++ spec/requests/api/v1/food_spec.rb
@@ -32,10 +32,15 @@ describe 'Food API' do
       expect(food[:name]).to eq('apple')
       expect(food[:calories]).to eq(100)
     end
+    it 'returns 404 if it cant find the food with corresponding id' do
+      get '/api/v1/foods/7'
+
+      expect(response.status).to eq(404)
+    end
   end
   context 'POST /api/v1/foods' do
     it 'creates a food' do
-      json_payload = {name: 'salad', calories: 50}
+      json_payload = { "food": {name: 'salad', calories: 50} }
       post '/api/v1/foods', params: json_payload
 
       expect(response).to be_successful
@@ -44,6 +49,38 @@ describe 'Food API' do
 
       expect(food[:name]).to eq('salad')
       expect(food[:calories]).to eq(50)
+      expect(Food.last[:name]).to eq('salad')
+      expect(Food.last[:calories]).to eq(50)
+    end
+    it 'returns 400 if it cant create a food' do
+      json_payload = { "food": {calories: 50} }
+      post '/api/v1/foods', params: json_payload
+
+      expect(response.status).to eq(400)
+    end
+  end
+  context 'PATCH /api/v1/foods/:id' do
+    it 'updates a food' do
+      expect(Food.find(3)[:name]).to eq('toast')
+      expect(Food.find(3)[:calories]).to eq(75)
+
+      json_payload = { "food": {name: 'toast', calories: 150} }
+      patch '/api/v1/foods/3', params: json_payload
+
+      expect(response).to be_successful
+
+      food = JSON.parse(response.body, symbolize_names: true)
+
+      expect(food[:name]).to eq('toast')
+      expect(food[:calories]).to eq(150)
+      expect(Food.find(3)[:name]).to eq('toast')
+      expect(Food.find(3)[:calories]).to eq(150)
+    end
+    it 'returns 400 if it cant update a food' do
+      json_payload = { "food": {calories: 150} }
+      patch '/api/v1/foods/3', params: json_payload
+
+      expect(response.status).to eq(400)
     end
   end
 end
